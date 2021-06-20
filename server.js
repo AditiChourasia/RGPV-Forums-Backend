3c66810868fdc3e74cdf87d3004ff03a1290d36e
LearnToCodeWithAditi <trivedivaidehi19@gmail.com>
2021-06-20 18:28:43 +0530

    Inititial Commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("API Running..."));
+
+app.listen(port, () => console.log("Listening on the port " + port));
