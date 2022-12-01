264269e3fb94b000db750dbfecdd3a59f9cca23a
LearnToCodeWithAditi <trivedivaidehi19@gmail.com>
2022-12-01 21:02:55 +0530

    msg

--- server.js
+++ server.js
@@ -31,4 +31,15 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-app.listen(5000, () => console.log("Listening on the port " + 5000));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
